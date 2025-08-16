a1efa872021300256a6ae96101968eef365afd9a
leavelet <leavelet@163.com>
2025-08-16 03:16:56 +0000

    fix: add crypo path

--- viorng/buildAll.bat
+++ viorng/buildAll.bat
@@ -1,4 +1,5 @@
 @echo off
+set INCLUDE=%INCLUDE%;F:\Windows Kits\10\Cryptographic Provider Development Kit\Include
 if "%VIRTIO_WIN_NO_ARM%"=="" call ..\build\build.bat viorng.sln "Win10 Win11" ARM64
 if errorlevel 1 goto :eof
 call ..\build\build.bat viorng.sln "Win10 Win11" %*
